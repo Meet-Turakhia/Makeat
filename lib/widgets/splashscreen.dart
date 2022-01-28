4abcabdb0bb688618ed44774a776771f69d9548c
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-01-28 15:51:14 +0530

    initial setup

---
+++ lib/widgets/splashscreen.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import "package:splashscreen/splashscreen.dart";
+import "package:google_fonts/google_fonts.dart";
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return SplashScreen(
+      seconds: 9,
+      backgroundColor: Colors.white,
+      image: Image.asset(
+        "assets/logo/makeat.gif",
+      ),
+      photoSize: 100.0,
+      title: Text("Makeat",
+          style:
+              GoogleFonts.ubuntu(fontSize: 30.0, fontWeight: FontWeight.bold)),
+      loadingText: Text(
+        "From \n nCoders",
+        style: GoogleFonts.ubuntu(fontSize: 15.0, color: Colors.grey),
+        textAlign: TextAlign.center,
+      ), //TextStyle(fontFamily: 'Open sans', color: Colors.black, fontSize: 25.0, fontWeight: FontWeight.bold),),
+      //styleTextUnderTheLoader: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black),
+      useLoader: false,
+      navigateAfterSeconds: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Makeat",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.transparent,
+      ),
+      // home: Home(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:makeat_app/main.dart';
+import 'package:makeat_app/widgets/splashscreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
