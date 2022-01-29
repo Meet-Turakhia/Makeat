731ecb2298cf2713c2d2c1146b715d75295abfef
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-01-30 01:28:59 +0530

    adding showtoast

---
+++ lib/widgets/showtoast.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: non_constant_identifier_names
+import "package:fluttertoast/fluttertoast.dart";
+import "package:flutter/material.dart";
+
+void displayMessage() {
+  Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
