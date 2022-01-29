d3193ece0b6ebc3b72c4127320ea12717189c70e
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-01-30 01:44:24 +0530

    customised toast pop up

--- lib/widgets/showtoast.dart
+++ lib/widgets/showtoast.dart
@@ -2,13 +2,13 @@
 import "package:fluttertoast/fluttertoast.dart";
 import "package:flutter/material.dart";
 
-void popupMessage() {
+void popupMessage(String msg) {
   Fluttertoast.showToast(
-      msg: "This is Center Short Toast",
+      msg: msg,
       toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.red,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 3,
+      backgroundColor: Colors.black,
       textColor: Colors.white,
       fontSize: 16.0);
 }
