0a50408b3623f9f2e436e2e04984cbbd68ef2ef2
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-02-05 16:43:48 +0530

    added some new fonts

--- lib/widgets/fonts.dart
+++ lib/widgets/fonts.dart
@@ -5,3 +5,7 @@ var mfont10 = GoogleFonts.ubuntu(color: Colors.black, fontSize: 10.0);
 var mfontw10 = GoogleFonts.ubuntu(color: Colors.white, fontSize: 10.0);
 var mfont15 = GoogleFonts.ubuntu(color: Colors.black, fontSize: 15.0);
 var mfontw15 = GoogleFonts.ubuntu(color: Colors.white, fontSize: 15.0);
+var mfont = GoogleFonts.ubuntu(color: Colors.black, fontSize: 15.0);
+var mfontw = GoogleFonts.ubuntu(color: Colors.white, fontSize: 15.0);
+var mfontl = GoogleFonts.ubuntu(fontSize: 17.0);
+var mfontwl = GoogleFonts.ubuntu(fontSize: 17.0);
