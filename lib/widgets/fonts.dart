263e7cb276e3f4f09b8a5051f1077a076e1b1eff
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-02-05 16:32:02 +0530

    adding fonts

---
+++ lib/widgets/fonts.dart
@@ -0,0 +1,7 @@
+import "package:google_fonts/google_fonts.dart";
+import "package:flutter/material.dart";
+
+var mfont10 = GoogleFonts.ubuntu(color: Colors.black, fontSize: 10.0);
+var mfontw10 = GoogleFonts.ubuntu(color: Colors.white, fontSize: 10.0);
+var mfont15 = GoogleFonts.ubuntu(color: Colors.black, fontSize: 15.0);
+var mfontw15 = GoogleFonts.ubuntu(color: Colors.white, fontSize: 15.0);
