de22266fb31e70561fe0a4988834d36857bcfb8d
Meet-Turakhia <turakhiameeturesh@gmail.com>
2022-02-08 16:54:51 +0530

    making recipe page dynamic part 5

--- lib/pages/recipe.dart
+++ lib/pages/recipe.dart
@@ -144,6 +144,10 @@ class _RecipeState extends State<Recipe> {
                   ingredients.add(
                       "${ingredientQuantities[i]!.replaceAll('"', '').replaceAll('NA', 'some')} ${ingredientNames[i]!.replaceAll('"', '')}");
                 }
+                List<String?> instructions = doubleQuotesPattern
+                    .allMatches(ds["RecipeInstructions"].toString())
+                    .map((e) => e.group(0))
+                    .toList();
                 return Stack(
                   children: [
                     recipeImage,
@@ -677,7 +681,7 @@ class _RecipeState extends State<Recipe> {
                                 height: 10,
                               ),
                               Text(
-                                ingredients.join(", "),
+                                "${ingredients.join(", ")}.",
                                 softWrap: true,
                                 style: mfont15,
                                 textAlign: TextAlign.justify,
@@ -695,23 +699,16 @@ class _RecipeState extends State<Recipe> {
                                 height: 10,
                               ),
                               Text(
-                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-                                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-                                'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in'
-                                'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-                                ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'
-                                'mollit anim id est laborum'
-                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-                                'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-                                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-                                'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in'
-                                'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-                                ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'
-                                'mollit anim id est laborum',
+                                instructions
+                                    .join()
+                                    .replaceAll('"', '')
+                                    .replaceAll('.', '. '),
                                 softWrap: true,
                                 style: mfont15,
                                 textAlign: TextAlign.justify,
+                              ),
+                              SizedBox(
+                                height: 10,
                               ),
                               Padding(
                                 padding: const EdgeInsets.all(8.0),
